80cd3f08d51a2a47ad85816d9de032eab0985a42
BenhrimaImad <imad.benhrima@gmail.com>
2019-07-29 19:04:32 +0100

    first  commit

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "debranding-project",
+
+    'summary': """
+        backend debranding module""",
+
+    'description': """
+       maxware company backend debranding module
+    """,
+
+    'author': "Maxware Technology",
+    'website': "http://www.maxware.ma",
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
